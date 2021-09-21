fd6aa9d7dc59e4a92cc97c444c8f718166ab4e12
junior <jose.junior@solucaosistemas.net>
2021-09-21 17:11:18 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/dev/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/dev/hrpayroll/resources/PaymentResource.java
@@ -1,4 +1,4 @@
-package com.dev.hrworker.resources;
+package com.dev.hrpayroll.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dev.hrworker.entities.Payment;
-import com.dev.hrworker.services.PaymentService;
+import com.dev.hrpayroll.entities.Payment;
+import com.dev.hrpayroll.services.PaymentService;
 
 @RestController
 @RequestMapping(value = "/payments")
@@ -16,10 +16,10 @@ public class PaymentResource {
 
 	@Autowired
 	private PaymentService service;
-	
-	@GetMapping(value = "/{workedId}/days/{days}")
-	public ResponseEntity<Payment> getPayment(@PathVariable Long workedId, @PathVariable Integer days) {
-		Payment payment = service.getPayment(workedId, days);
+
+	@GetMapping(value = "/{workerId}/days/{days}")
+	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
+		Payment payment = service.getPayment(workerId, days);
 		return ResponseEntity.ok(payment);
-	}
+	}	
 }

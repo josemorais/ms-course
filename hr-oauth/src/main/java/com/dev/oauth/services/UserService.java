70ef0c4ea953d752b98409dedac511a54e44ac43
junior <jose.junior@solucaosistemas.net>
2021-09-24 20:41:05 -0300

    UserFeignClient

---
+++ hr-oauth/src/main/java/com/dev/oauth/services/UserService.java
@@ -0,0 +1,31 @@
+package com.dev.oauth.services;
+
+import java.util.Objects;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dev.oauth.entities.User;
+import com.dev.oauth.feignclients.UserFeignClient;
+
+@Service
+public class UserService {
+	
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+	
+	@Autowired
+	private UserFeignClient userFeignClient;
+	
+	public User findByEmail(String email) {
+		User user = userFeignClient.findByEmail(email).getBody();
+		if (Objects.isNull(user)) {
+			logger.error("Email not found: " + email);
+			throw new IllegalArgumentException("Email not found");
+		}
+		logger.info("Email found: " + email);
+		return user;
+	}
+
+}
